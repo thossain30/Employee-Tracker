65d5ed4e24e969390695b672a17bd6437d8b6928
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-17 19:00:26 -0400

    Added foundational code for inquirer

--- main/db/schema.sql
+++ main/db/schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(8,2) NOT NULL,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id)
     REFERENCES department(id) 
 );
--- main/index.js
+++ main/index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const cTable = require("console.table");
 require("dotenv").config();
 
 const db = mysql.createConnection(
@@ -10,3 +11,59 @@ const db = mysql.createConnection(
         database: "employees_db"
     }
 )
+
+const questions = [
+    {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Department",
+            "Quit"
+        ],
+        name: "task" 
+    }
+]
+
+function runTask(task) {
+    switch(task) {
+        case "View All Employees": 
+            //
+            break;
+        case "Add Employee": 
+            //
+            break;
+        case "Update Employee Role": 
+            //
+            break;
+        case "View All Roles": 
+            //
+            break;
+        case "Add Role": 
+            //
+            break;
+        case "View All Departments": 
+            //
+            break;
+        case "Add Department": 
+            //
+            break;
+        case "Quit": 
+            //
+            break;
+    }
+}
+
+function init() {
+    inquirer.prompt(questions).
+    then(answers => {
+        runTask(answers.task);
+    })
+}
+
+init();
