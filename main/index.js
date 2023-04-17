9dba3681378090e98f36f9da07f2dad0c42593de
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-17 18:07:07 -0400

    created connection to SQL

--- main/index.js
+++ main/index.js
@@ -1,2 +1,12 @@
 const inquirer = require("inquirer");
-const mysql2 = require("mysql2");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: process.env.USER,
+        password: process.env.PASS,
+        database: "employees_db"
+    }
+)
