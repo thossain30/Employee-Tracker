835f3cbde69af27970924e2df4ddc3d7ace065d9
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-04-18 19:26:28 -0400

    add role method implemented

--- main/index.js
+++ main/index.js
@@ -7,15 +7,17 @@ const db = mysql.createConnection(
     {
         host: "localhost",
         port: process.env.PORT || 3306,
-        user: process.env.USER,
-        password: process.env.PASS,
+        user: process.env.USER || "root",
+        password: process.env.PASS || "", 
         database: "employees_db"
-    }
+    },
+    console.log("Connected to mysql!")
 )
 
-const questions = [
+const question = [
     {
         type: "list",
+        name: "task",
         message: "What would you like to do?",
         choices: [
             "View All Employees",
@@ -26,13 +28,12 @@ const questions = [
             "View All Departments",
             "Add Department",
             "Quit"
-        ],
-        name: "task" 
+        ]
     }
 ]
 
 function viewAllEmployees() {
-    db.query("SELECT * FROM Employees", (err, results) => {
+    db.query("SELECT * FROM Employees;", (err, results) => {
         if (err) {
             console.log(err);
         }
@@ -41,7 +42,7 @@ function viewAllEmployees() {
 }
 
 function viewAllRoles() {
-    db.query("SELECT * FROM Role", (err, results) => {
+    db.query("SELECT * FROM Role;", (err, results) => {
         if (err) {
             console.log(err);
         }
@@ -49,8 +50,43 @@ function viewAllRoles() {
     })
 }
 
+function addRole() {
+    db.query("SELECT * FROM Department;", (err,res) => {
+        if (err) console.log(err);
+        let departments = res.map(department => ({name: department.department_name, id: department.id}));
+        inquirer.prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is the name of the role you want to add?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary of the role you want to add?"
+            },
+            {
+                name: "deptName",
+                type: "list",
+                message: "Which department do you want to add the new role to?",
+                choices: departments
+            }
+        ]).then((response) => {
+            db.query(`INSERT INTO Role SET ?` ,
+            {
+                title: response.title,
+                salary: response.salary,
+                department_id: response.deptName,
+            }, (err,res) => {
+                if (err) console.log(err)
+                console.log(`\n ${response.title} successfully added to database! \n`);
+            })
+        }) 
+    })
+};
+
 function viewAllDepartments() {
-    db.query("SELECT * FROM Department", (err, results) => {
+    db.query("SELECT * FROM Department;", (err, results) => {
         if (err) {
             console.log(err);
         }
@@ -72,46 +108,72 @@ function addDepartment() {
         },
         (err, res) => {
             if (err) {console.log(err);}
-            console.log(`\n ${response.newDept} successfully added to database! \n`);
+            console.log(`\n ${res.newDept} successfully added to database! \n`);
         })
     })
 }
 
-function runTask(task) {
-    switch(task) {
-        case "View All Employees": 
-            viewAllEmployees();
-            break;
-        case "Add Employee": 
-            //
-            break;
-        case "Update Employee Role": 
-            //
-            break;
-        case "View All Roles": 
-            viewAllRoles();
-            break;
-        case "Add Role": 
-            //
-            break;
-        case "View All Departments": 
-            viewAllDepartments();
-            break;
-        case "Add Department": 
-            addDepartment();
-            break;
-        case "Quit": 
-            db.end();
-            console.log('\n You have now exited the employee management program. \n');
-            break;
-    }
-}
+// function runTask(task) {
+//     switch(task) {
+//         case "View All Employees": 
+//             viewAllEmployees();
+//             break;
+//         case "Add Employee": 
+//             //
+//             break;
+//         case "Update Employee Role": 
+//             //
+//             break;
+//         case "View All Roles": 
+//             viewAllRoles();
+//             break;
+//         case "Add Role": 
+//             //
+//             break;
+//         case "View All Departments": 
+//             viewAllDepartments();
+//             break;
+//         case "Add Department": 
+//             addDepartment();
+//             break;
+//         case "Quit": 
+//             db.end();
+//             console.log('\n You have now exited the employee management program. \n');
+//             break;
+//     }
+// }
 
 function init() {
-    inquirer.prompt(questions).
-    then(answer => {
-        runTask(answer.task);
-    })
+    inquirer.prompt(question).then
+        ((answer) => {
+        switch(answer.task) {
+            case "View All Employees": 
+                viewAllEmployees();
+                break;
+            case "Add Employee": 
+                //
+                break;
+            case "Update Employee Role": 
+                //
+                break;
+            case "View All Roles": 
+                viewAllRoles();
+                break;
+            case "Add Role": 
+                addRole();
+                break;
+            case "View All Departments": 
+                viewAllDepartments();
+                break;
+            case "Add Department": 
+                addDepartment();
+                break;
+            case "Quit": 
+                db.end();
+                console.log('\n You have now exited the employee management program. \n');
+                break;
+        }
+    });
 }
 
 init();
